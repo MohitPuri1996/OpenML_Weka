fea67b84c2e9621a42a3cdc568ad432ff5fa2b57
Jan N. van Rijn <janvanrijn@gmail.com>
2013-05-08 11:09:48 +0200

    incorperates task_id in result arff

--- main/java/Weka/src/openml/experiment/TaskResultProducer.java
+++ main/java/Weka/src/openml/experiment/TaskResultProducer.java
@@ -17,43 +17,89 @@ import weka.experiment.OutputZipper;
 public class TaskResultProducer extends CrossValidationResultProducer {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final String FOLDS_FILE_TRAIN = "TRAIN";
 	private static final String FOLDS_FILE_TEST = "TEST";
 	
+	private static final String TASK_FIELD_NAME = "OpenML_Task_id";
+
 	/** The task to be run */
 	protected Task m_Task;
-	
+
 	/** Instances file with splits in it **/
 	protected Instances m_Splits;
-	
+
 	public TaskResultProducer() {
 		super();
 
 		if (getSplitEvaluator() instanceof ClassifierSplitEvaluator) {
-			((ClassifierSplitEvaluator) getSplitEvaluator()).setPredTargetColumn(true);
+			((ClassifierSplitEvaluator) getSplitEvaluator())
+					.setPredTargetColumn(true);
 		}
 	}
-	
-	public void setTask (Task t) throws Exception {
+
+	public void setTask(Task t) throws Exception {
 		m_Task = t;
-		
+
 		Data_set ds = TaskInformation.getSourceData(m_Task);
-		Estimation_procedure ep = TaskInformation.getEstimationProcedure(m_Task);
-		
-		DataSetDescription dsd = ApiConnector.openmlDataDescription(ds.getData_set_id());
+		Estimation_procedure ep = TaskInformation
+				.getEstimationProcedure(m_Task);
+
+		DataSetDescription dsd = ApiConnector.openmlDataDescription(ds
+				.getData_set_id());
 		m_Instances = ApiConnector.getDatasetFromUrl(dsd.getUrl());
 		InstancesHelper.setTargetAttribute(m_Instances, ds.getTarget_feature());
 		m_Splits = ApiConnector.getDatasetFromUrl(ep.getData_splits_url());
 	}
-	
+
+	/**
+	 * Gets the names of each of the columns produced for a single run. This
+	 * method should really be static.
+	 * 
+	 * @return an array containing the name of each column
+	 */
+	@Override
+	public String[] getKeyNames() {
+
+		String[] keyNames = m_SplitEvaluator.getKeyNames();
+		// Add in the names of our extra key fields
+		String[] newKeyNames = new String[keyNames.length + 4];
+		newKeyNames[0] = DATASET_FIELD_NAME;
+		newKeyNames[1] = RUN_FIELD_NAME;
+		newKeyNames[2] = FOLD_FIELD_NAME;
+		newKeyNames[3] = TASK_FIELD_NAME;
+		System.arraycopy(keyNames, 0, newKeyNames, 4, keyNames.length);
+		return newKeyNames;
+	}
+
+	/**
+	 * Gets the data types of each of the columns produced for a single run.
+	 * This method should really be static.
+	 * 
+	 * @return an array containing objects of the type of each column. The
+	 *         objects should be Strings, or Doubles.
+	 */
+	@Override
+	public Object[] getKeyTypes() {
+
+		Object[] keyTypes = m_SplitEvaluator.getKeyTypes();
+		// Add in the types of our extra fields
+		Object[] newKeyTypes = new String[keyTypes.length + 4];
+		newKeyTypes[0] = new String();
+		newKeyTypes[1] = new String();
+		newKeyTypes[2] = new String();
+		newKeyTypes[3] = new String();
+		System.arraycopy(keyTypes, 0, newKeyTypes, 4, keyTypes.length);
+		return newKeyTypes;
+	}
+
 	@Override
 	public void doRun(int run) throws Exception {
 		int attTypeIndex = m_Splits.attribute("type").index();
 		int attRowidIndex = m_Splits.attribute("rowid").index();
 		int attFoldIndex = m_Splits.attribute("fold").index();
 		int attRepeatIndex = m_Splits.attribute("repeat").index();
-		
+
 		if (getRawOutput()) {
 			if (m_ZipDest == null) {
 				m_ZipDest = new OutputZipper(m_OutputFile);
@@ -63,51 +109,56 @@ public class TaskResultProducer extends CrossValidationResultProducer {
 		if (m_Instances == null) {
 			throw new Exception("No Instances set");
 		}
-		
-		if(m_Task == null) {
+
+		if (m_Task == null) {
 			throw new Exception("No task set");
 		}
-		
+
 		// creating al empty copies for each fold
 		Instances[] trainingSets = new Instances[m_NumFolds];
 		Instances[] testSets = new Instances[m_NumFolds];
-		
-		for(int i = 0; i < m_NumFolds; ++i) {
+
+		for (int i = 0; i < m_NumFolds; ++i) {
 			trainingSets[i] = new Instances(m_Instances, 0, 0);
 			testSets[i] = new Instances(m_Instances, 0, 0);
 		}
-		for(int i = 0; i < m_Splits.numInstances(); ++i) {
+		for (int i = 0; i < m_Splits.numInstances(); ++i) {
 			int repeat = (int) m_Splits.instance(i).value(attRepeatIndex);
-			if(repeat == run - 1) { // 1based - 0based correction
+			if (repeat == run - 1) { // 1based - 0based correction
 				int fold = (int) m_Splits.instance(i).value(attFoldIndex);
-				String type = m_Splits.attribute(attTypeIndex).value((int) m_Splits.instance(i).value(attTypeIndex));
-				int rowid = Integer.parseInt( m_Splits.attribute(attRowidIndex).value((int) m_Splits.instance(i).value(attRowidIndex)));
-				
-				if(type.equals(FOLDS_FILE_TRAIN)) {
+				String type = m_Splits.attribute(attTypeIndex).value(
+						(int) m_Splits.instance(i).value(attTypeIndex));
+				int rowid = Integer
+						.parseInt(m_Splits.attribute(attRowidIndex)
+								.value((int) m_Splits.instance(i).value(
+										attRowidIndex)));
+
+				if (type.equals(FOLDS_FILE_TRAIN)) {
 					trainingSets[fold].add(m_Instances.instance(rowid));
-				} else if(type.equals(FOLDS_FILE_TEST)) {
+				} else if (type.equals(FOLDS_FILE_TEST)) {
 					testSets[fold].add(m_Instances.instance(rowid));
 				}
 			}
 		}
-		
+
 		for (int fold = 0; fold < m_NumFolds; fold++) {
 			// Add in some fields to the key like run and fold number, dataset
 			// name
 			Object[] seKey = m_SplitEvaluator.getKey();
-			Object[] key = new Object[seKey.length + 3];
+			Object[] key = new Object[seKey.length + 4];
 			key[0] = Utils.backQuoteChars(m_Instances.relationName());
 			key[1] = "" + run;
 			key[2] = "" + (fold + 1);
-			System.arraycopy(seKey, 0, key, 3, seKey.length);
+			key[3] = "" + m_Task.getTask_id();
+			System.arraycopy(seKey, 0, key, 4, seKey.length);
 			if (m_ResultListener.isResultRequired(this, key)) {
 				try {
-					Object[] seResults = m_SplitEvaluator
-							.getResult(trainingSets[fold], testSets[fold]);
+					Object[] seResults = m_SplitEvaluator.getResult(
+							trainingSets[fold], testSets[fold]);
 					Object[] results = new Object[seResults.length + 1];
 					results[0] = getTimestamp();
 					System.arraycopy(seResults, 0, results, 1, seResults.length);
-					
+
 					m_ResultListener.acceptResult(this, key, results);
 				} catch (Exception ex) {
 					// Save the train and test datasets for debugging purposes?
