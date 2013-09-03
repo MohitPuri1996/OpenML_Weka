9872b3a1976d36aedd29d09c688673f0a4020d02
Jan van Rijn <janvanrijn@gmail.com>
2013-09-03 13:38:01 +0200

    changed namespace of xml to openml.org (without the dash)

--- main/java/Weka/src/openml/constants/Constants.java
+++ main/java/Weka/src/openml/constants/Constants.java
@@ -3,5 +3,5 @@ package openml.constants;
 public class Constants {
 	
 	public static final String DATASET_FORMAT = ".arff";
-	public static final String OPENML_XMLNS = "http://open-ml.org/openml";
+	public static final String OPENML_XMLNS = "http://openml.org/openml";
 }
